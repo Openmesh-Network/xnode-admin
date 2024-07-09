5f1087ddf83e87ca89acd361705eb3647d37548d
harrys522 <hsackman@protonmail.com>
2024-07-09 10:35:39 +1000

    fixed run local script

--- run-local.sh
+++ run-local.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python src/xnode_admin/tests/rebuild_tests.py ./mock_studio_message_v2.json &
+python src/xnode_admin/tests/mock_studio_tests.py ./mock_studio_message_v2.json &
 python src/xnode_admin/main.py --remote http://localhost:5000/xnodes/functions --uuid I5KMFECV11H-VX5K78G4P7I --access-token Tah6WlMnal0mpka6ki8jHmoD9hhK9KXc81xyNjvSt1hm1nj74dlM4W8jPEdPdmSJD1JVba+eDHEceUysRZnplw== ./
