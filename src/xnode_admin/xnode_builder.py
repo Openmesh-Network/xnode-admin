264794d59880183361d480838220591d846d3a19
dowdt <tomas.dowd@openmesh.network>
2024-07-26 15:25:20 +1000

    Slightly increased update interval to test channel update.

--- src/xnode_admin/xnode_builder.py
+++ src/xnode_admin/xnode_builder.py
@@ -174,7 +174,7 @@ def fetch_config_studio(studio_url, xnode_uuid, access_token, state_directory):
     hearbeat_interval = 30 # Heartbeat interval in seconds.
     generation_interval = 10 # API call to dpl to check if there's a new config or a new update.
     # XXX: Increase this interval to once every few hours, because it blocks for ~30 seconds in the best case.
-    update_interval = 100 # How often to check if there's an update on the current channel.
+    update_interval = 120 # How often to check if there's an update on the current channel.
     cpu_usage_list = []
     mem_usage_list = []
     preshared_key = base64.b64decode(access_token).hex()
